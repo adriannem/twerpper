ae1abb3bfa4b92ea527d9806b05f43efd43159b4
Adrianne M <affirmwealth@gmail.com>
2014-07-14 08:41:33 -0500

    added Twitter bootstrap, form, sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  resources :sessions
+
   resources :users
+
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  root to: "users#new"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
