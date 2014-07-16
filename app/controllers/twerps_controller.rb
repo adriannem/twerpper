7a2934c724497ea1e1174036f2d7ba71fd36ca4d
Adrianne M <affirmwealth@gmail.com>
2014-07-16 06:40:58 -0500

    now saving twerps

--- app/controllers/twerps_controller.rb
+++ app/controllers/twerps_controller.rb
@@ -1,2 +1,11 @@
 class TwerpsController < ApplicationController
+
+	def create
+		twerp = Twerp.new(params[:twerp])
+		twerp.user_id = current_user.id
+	
+
+		flash[:error] = "Your Twerp was over 140 characters" unless twerp.save
+		redirect_to root_url
+
 end
